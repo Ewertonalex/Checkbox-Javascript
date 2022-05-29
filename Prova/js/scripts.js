749bc417b93edb0b2867c4b258acecba6d61c173
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2022-05-28 21:55:04 -0300

    Add files via upload

---
+++ Prova/js/scripts.js
@@ -0,0 +1,27 @@
+var checkBoxes = document.querySelectorAll(".checkbox");
+
+var selecionados = 0;
+
+var btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    selecionados = 0;
+
+    checkBoxes.forEach(function(el) {
+
+        if(el.checked) {
+            selecionados++;
+        }        
+
+    });
+
+    console.log(selecionados)
+    alert("Você selecionou " + selecionados + " compromissos!");
+    
+
+});
+
+
